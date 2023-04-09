60ebd9375d15bc98846d2d58cc7033cc5a487377
MaiconFiuza <maicon.b.fiuza@gmail.com>
2023-04-09 19:16:02 -0300

    :sparkles: create customer dataclass e dto

--- src/main/kotlin/fiuza/maicon/mercado_livro/domain/Customer.kt
+++ src/main/kotlin/fiuza/maicon/mercado_livro/domain/Customer.kt
@@ -1,6 +1,9 @@
 package fiuza.maicon.mercado_livro.domain
 
+import org.springframework.data.annotation.Id
+
 data class Customer(
+    @Id
     val id: String,
     val name: String,
     val email: String
---
+++ src/main/kotlin/fiuza/maicon/mercado_livro/domain/dto/CustomerDto.kt
@@ -0,0 +1,6 @@
+package fiuza.maicon.mercado_livro.domain.dto
+
+data class CustomerDto(
+    val name: String,
+    val email: String
+)
