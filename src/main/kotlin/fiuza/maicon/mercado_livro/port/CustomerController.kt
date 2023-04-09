7792e025732d8f66e097e561a6bb140bab66e647
MaiconFiuza <maicon.b.fiuza@gmail.com>
2023-04-09 19:16:46 -0300

    :sparkles: cria a controller do customer

--- src/main/kotlin/fiuza/maicon/mercado_livro/port/CustomerController.kt
+++ src/main/kotlin/fiuza/maicon/mercado_livro/port/CustomerController.kt
@@ -1,7 +1,10 @@
 package fiuza.maicon.mercado_livro.port
 
 import fiuza.maicon.mercado_livro.domain.Customer
+import fiuza.maicon.mercado_livro.domain.dto.CustomerDto
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,5 +20,10 @@ class CustomerController {
             name = "Fifiuza",
             email = "maicon.b.fiuza@gmail.com"
         )
+    }
+
+    @PostMapping
+    fun createCustomer(@RequestBody customer: CustomerDto) {
+       println(customer)
     }
 }
