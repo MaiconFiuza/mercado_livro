4e65507cfe979b2545dfabde8a9c5a99ee869b3c
MaiconFiuza <maicon.b.fiuza@gmail.com>
2023-04-13 21:51:04 -0300

    :art: altera o status code do create customer

--- src/main/kotlin/fiuza/maicon/mercado_livro/port/CustomerController.kt
+++ src/main/kotlin/fiuza/maicon/mercado_livro/port/CustomerController.kt
@@ -2,11 +2,13 @@ package fiuza.maicon.mercado_livro.port
 
 import fiuza.maicon.mercado_livro.domain.Customer
 import fiuza.maicon.mercado_livro.domain.dto.CustomerDto
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -23,6 +25,7 @@ class CustomerController {
     }
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED )
     fun createCustomer(@RequestBody customer: CustomerDto) {
        println(customer)
     }
